67e6b785045bb3cfef0acbb29bca262946d5ed97
Max Bo <max@maxbo.me>
2017-11-24 15:51:11 +1000

    Add yielding

--- src/Caucus.tsx
+++ src/Caucus.tsx
@@ -140,32 +140,18 @@ function CaucusMeta(props: { data: CaucusData, fref: firebase.database.Reference
   );
 }
 
-function CaucusNowSpeaking(props: { data: CaucusData, fref: firebase.database.Reference }) {
-  return (
-    <div>
-      <Header as="h3" attached="top">Now Speaking</Header>
-      <Segment attached="bottom">
-        <Feed size="large">
-          <SpeakerEvent data={props.data.speaking} fref={props.fref.child('speaking')} />
-        </Feed>
-      </Segment>
-    </div>
-  );
-}
-
 interface Lifecycle {
   history: firebase.database.Reference;
   speakingData?: SpeakerEvent;
   speaking: firebase.database.Reference;
-  timerData: TimerData; 
+  timerData: TimerData;
   timer: firebase.database.Reference;
   yielding: boolean;
-  queueHeadData?: SpeakerEvent; 
+  queueHeadData?: SpeakerEvent;
   queueHead?: firebase.database.Reference;
 }
 
 function runLifecycle(lifecycle: Lifecycle) {
-
   const { history, speakingData, speaking, timerData, timer, yielding, queueHeadData, queueHead } = lifecycle;
 
   let additionalYieldTime = 0;
@@ -187,7 +173,10 @@ function runLifecycle(lifecycle: Lifecycle) {
   } // do nothing if no-one is currently speaking
 
   if (queueHead && queueHeadData) {
-    speaking.set(queueHeadData);
+    speaking.set({
+      ...queueHeadData,
+      duration: queueHeadData.duration + additionalYieldTime
+    });
 
     timer.update({
       elapsed: 0,
@@ -197,50 +186,6 @@ function runLifecycle(lifecycle: Lifecycle) {
 
     queueHead.set(null);
   }
-}
-
-
-const SpeakerEvent = (props: { data?: SpeakerEvent, fref: firebase.database.Reference }) => {
-  const makeHandler = (field: string) => (e: React.FormEvent<HTMLInputElement>) =>
-    props.fref.child(field).set(e.currentTarget.value);
-
-  return props.data ? (
-    <Feed.Event>
-      {/* <Feed.Label image='/assets/images/avatar/small/helen.jpg' /> */}
-      <Feed.Content>
-        <Feed.Summary>
-          <Feed.User>
-            <Flag name={props.data.who.toLowerCase() as any} />
-            {props.data.who}
-          </Feed.User>
-          <Feed.Date>{props.data.duration.toString() + ' seconds'}</Feed.Date>
-        </Feed.Summary>
-        <Feed.Meta>
-          <Feed.Like>
-            <StanceIcon stance={props.data.stance} />
-            {props.data.stance}
-          </Feed.Like>
-          <Label size="mini" as="a" onClick={() => props.fref.remove()}>
-            Remove
-          </Label>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
-  ) : <Feed.Event />;
-};
-
-function SpeakerEvents(props: { data?: Map<string, SpeakerEvent>, fref: firebase.database.Reference }) {
-  const events = props.data ? props.data : {};
-
-  const eventItems = Object.keys(events).map(key =>
-    <SpeakerEvent key={key} data={events[key]} fref={props.fref.child(key)} />
-  );
-
-  return (
-    <Feed size="large">
-      {eventItems}
-    </Feed>
-  );
 }
 
 export class Caucus extends React.Component<Props, State> {
@@ -294,6 +239,102 @@ export class Caucus extends React.Component<Props, State> {
     );
   }
 
+  SpeakerEvent = (local: { 
+    data?: SpeakerEvent, 
+    speaking?: SpeakerEvent,
+    fref: firebase.database.Reference, 
+  }) => {
+    const yieldHandler = () => {
+      const queueHeadDetails = {
+        queueHeadData: local.data,
+        queueHead: local.fref
+      };
+
+      // HACK
+      // HERE BE DRAGONS
+      // The only reason I'm doing this is because I honestly couldn't give a shit about propogating
+      // the caucusRef all the way down. Furthermore, the only time this should ever be called is when the 
+      // SpeakerEvent is in the "queue" zone, meaning we'll pop up into the "caucus" field.
+      const caucusRef = (local.fref.parent as firebase.database.Reference).parent as firebase.database.Reference;
+
+      const lifecycle: Lifecycle = {
+        history: caucusRef.child('history'),
+        speaking: caucusRef.child('speaking'),
+        speakingData: local.speaking,
+        timerData: this.state.speakerTimer,
+        timer: caucusRef.child('speakerTimer'),
+        yielding: true,
+      };
+
+      runLifecycle({ ...lifecycle, ...queueHeadDetails });
+    };
+
+    return local.data ? (
+      <Feed.Event>
+        {/* <Feed.Label image='/assets/images/avatar/small/helen.jpg' /> */}
+        <Feed.Content>
+          <Feed.Summary>
+            <Feed.User>
+              <Flag name={local.data.who.toLowerCase() as any} />
+              {local.data.who}
+            </Feed.User>
+            <Feed.Date>{local.data.duration.toString() + ' seconds'}</Feed.Date>
+          </Feed.Summary>
+          <Feed.Meta>
+            <Feed.Like>
+              <StanceIcon stance={local.data.stance} />
+              {local.data.stance}
+            </Feed.Like>
+            <Label size="mini" as="a" onClick={() => local.fref.remove()}>
+              Remove
+            </Label>
+            {local.speaking && (<Label size="mini" as="a" onClick={yieldHandler}>
+              Yield
+            </Label>)}
+          </Feed.Meta>
+        </Feed.Content>
+      </Feed.Event>
+    ) : <Feed.Event />;
+  }
+
+  SpeakerEvents = (props: {
+    data?: Map<string, SpeakerEvent>,
+    fref: firebase.database.Reference,
+    speaking?: SpeakerEvent
+  }) => {
+    const events = props.data ? props.data : {};
+
+    const eventItems = Object.keys(events).map(key =>
+      (
+      <this.SpeakerEvent 
+        key={key} 
+        data={events[key]} 
+        fref={props.fref.child(key)} 
+        speaking={props.speaking}
+      />
+      )
+    );
+
+    return (
+      <Feed size="large">
+        {eventItems}
+      </Feed>
+    );
+  }
+
+  CaucusNowSpeaking = (props: { data: CaucusData, fref: firebase.database.Reference }) => {
+    return (
+      <div>
+        <Header as="h3" attached="top">Now Speaking</Header>
+        <Segment attached="bottom">
+          <Feed size="large">
+            <this.SpeakerEvent data={props.data.speaking} fref={props.fref.child('speaking')} />
+          </Feed>
+        </Segment>
+      </div>
+    );
+  }
+
   CaucusNextSpeaking = (props: { data: CaucusData, fref: firebase.database.Reference }) => {
     const nextSpeaker = () => {
 
@@ -304,7 +345,7 @@ export class Caucus extends React.Component<Props, State> {
       let queueHeadDetails = {};
 
       if (queueHeadKey) {
-        queueHeadDetails = { 
+        queueHeadDetails = {
           queueHeadData: queue[queueHeadKey],
           queueHead: props.fref.child('queue').child(queueHeadKey)
         };
@@ -318,7 +359,7 @@ export class Caucus extends React.Component<Props, State> {
         timer: props.fref.child('speakerTimer'),
         yielding: false,
       };
-      
+
       runLifecycle({ ...lifecycle, ...queueHeadDetails });
     };
 
@@ -326,7 +367,7 @@ export class Caucus extends React.Component<Props, State> {
       <div>
         <Header as="h3" attached="top">Next Speaking</Header>
         <Segment attached>
-          <SpeakerEvents data={props.data.queue} fref={props.fref.child('queue')} />
+          <this.SpeakerEvents data={props.data.queue} fref={props.fref.child('queue')} speaking={props.data.speaking} />
         </Segment>
         <Segment attached="bottom">
           <Button
@@ -369,24 +410,24 @@ export class Caucus extends React.Component<Props, State> {
         </Grid.Row>
         <Grid.Row>
           <Grid.Column>
-            <CaucusNowSpeaking data={props.data} fref={props.fref} />
+            <this.CaucusNowSpeaking data={props.data} fref={props.fref} />
             <this.CaucusNextSpeaking data={props.data} fref={props.fref} />
             <this.CaucusQueuer data={props.data} members={members} fref={props.fref} />
           </Grid.Column>
           <Grid.Column>
-            <Timer 
+            <Timer
               value={this.state.speakerTimer}
-              name="Speaker Timer" 
-              fref={props.fref.child('speakerTimer')} 
+              name="Speaker Timer"
+              fref={props.fref.child('speakerTimer')}
               key={props.caucusID + 'speakerTimer'}
-              onChange={(timer) => this.setState( { speakerTimer: timer })}
+              onChange={(timer) => this.setState({ speakerTimer: timer })}
             />
-            <Timer 
+            <Timer
               value={this.state.caucusTimer}
-              name="Caucus Timer" 
-              fref={props.fref.child('caucusTimer')} 
-              key={props.caucusID + 'caucusTimer'} 
-              onChange={(timer) => this.setState( { caucusTimer: timer })}
+              name="Caucus Timer"
+              fref={props.fref.child('caucusTimer')}
+              key={props.caucusID + 'caucusTimer'}
+              onChange={(timer) => this.setState({ caucusTimer: timer })}
             />
           </Grid.Column>
         </Grid.Row>
