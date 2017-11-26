3b59d7355f586d31e755f6bc2dd30bb7de263482
Max Bo <max@maxbo.me>
2017-11-27 04:48:33 +1000

    Update footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Committee from './Committee';
 const Footer = () => {
   return (
     <Message compact size="mini">
-      Made with 💖 by <a href="http://maxbo.me/">Max Bo</a>
+      Made with 💖 by <a href="https://github.com/MaxwellBo">Max Bo</a>
       {/* , <a href="http://hugokawamata.com/">Hugo Kawamata</a>, 
       and <a href="http://charltongroves.com/">Charlie Groves</a> */}
     </Message>);
