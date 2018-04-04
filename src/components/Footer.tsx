6333ba99aa1d5c6ef18afd4c19f5afa895efd68c
Max Bo <max@maxbo.me>
2018-04-04 21:02:51 +1000

    Check remote for new versions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,20 +2,53 @@ import * as React from 'react';
 
 import { Message } from 'semantic-ui-react';
 
-export class Footer extends React.PureComponent<{}, {}> {
+interface State {
+  latestVersion?: string;
+}
 
-  version = (
-    <a href="https://github.com/MaxwellBo/Muncoordinated-2/releases">
-      v2.1.1
-    </a>
-  );
+const CLIENT_VERSION = 'v2.1.1'
+const RELEASES_LATEST = 'https://api.github.com/repos/MaxwellBo/Muncoordinated-2/releases/latest';
+
+export class Footer extends React.PureComponent<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {};
+  }
+
+  fetchLatestVersion = (): Promise<void> => {
+    this.setState({ latestVersion: undefined });
+
+    return fetch(RELEASES_LATEST).then(response =>
+      response.json()
+    ).then(json => 
+      this.setState({ latestVersion: json.tag_name })
+    );
+  }
+
+  componentDidMount() {
+    this.fetchLatestVersion();
+  }
 
   render() {
-    const { version } = this;
+    const { latestVersion } = this.state;
+
+    const version = (
+      <a href="https://github.com/MaxwellBo/Muncoordinated-2/releases">
+        {CLIENT_VERSION}
+      </a>
+    );
+
+    const refreshNudge = (
+      <Message.List>
+        <Message.Item>New version available - refresh your browser</Message.Item>
+      </Message.List>
+    );
 
     return (
-      <Message compact size="mini">
-        Made with 😡 by <a href="https://github.com/MaxwellBo">Max Bo</a>. {version}
+      <Message compact size="mini" loading={!!latestVersion} list={["HMM"]}>
+        Made with 💖 by <a href="https://github.com/MaxwellBo">Max Bo</a>. {version}
+        {latestVersion !== CLIENT_VERSION && refreshNudge}
       </Message>
     );
   }
