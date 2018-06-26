158c7901cd26306737b0c3cf4f7178b73868d243
Max Bo <max@maxbo.me>
2018-06-26 11:13:39 +1000

    Update connection status failure message

--- src/components/ConnectionStatus.tsx
+++ src/components/ConnectionStatus.tsx
@@ -51,7 +51,7 @@ export default class ConnectionStatus extends React.Component<Props, State> {
         <Icon name="circle notched" loading />
         <Message.Content>
           <Message.Header>Connection Lost</Message.Header>
-          Refresh the page, as local changes will no longer be committed to the server. You will need to login again.
+          Refresh the page; local changes are no longer being committed to the server. You will need to login again.
         </Message.Content>
       </Message>
     ) : <div />;
