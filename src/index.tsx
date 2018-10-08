f590f248b2fe5b6b13e8bf747f6e7a5946df2a4b
Max Bo <max@maxbo.me>
2018-10-08 13:49:10 +1000

    Add Sentry error logging

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import createHistory from 'history/createBrowserHistory';
 import * as ReactGA from 'react-ga';
+import * as Sentry from '@sentry/browser';
 import 'semantic-ui-css/semantic.min.css';
 
+Sentry.init({ dsn: 'https://1e4f23a087974ab482cacf50699c6dbd@sentry.io/1296285' });
+Sentry.captureMessage('test message');
 ReactGA.initialize('UA-122177622-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
