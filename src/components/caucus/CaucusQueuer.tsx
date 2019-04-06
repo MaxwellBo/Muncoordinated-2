7968a1c4a81a18b59a2e7394ba5da2cd3a8a49eb
Max Bo <mbo@atlassian.com>
2019-04-06 12:06:22 +1100

    Hookify CaucusQueuer

--- src/components/caucus/CaucusQueuer.tsx
+++ src/components/caucus/CaucusQueuer.tsx
@@ -16,20 +16,12 @@ interface Props {
   caucusFref: firebase.database.Reference;
 }
 
-interface State {
-  queueMember?: MemberOption;
-}
+export default function CaucusQueuer(props: Props) {
+  const { members, caucus, caucusFref } = props;
+  const [queueMember, setQueueMember] = React.useState<MemberOption | undefined>(undefined);
 
-export default class CaucusQueuer extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  setStance = (stance: Stance) => () => {
-    const { queueMember } = this.state;
-    const { caucus } = this.props;
+  const setStance = (stance: Stance) => () => {
+    const { caucus } = props;
 
     const duration = Number(recoverDuration(caucus));
 
@@ -40,77 +32,70 @@ export default class CaucusQueuer extends React.Component<Props, State> {
         duration: recoverUnit(caucus) === Unit.Minutes ? duration * 60 : duration,
       };
 
-      this.props.caucusFref.child('queue').push().set(newEvent);
+      props.caucusFref.child('queue').push().set(newEvent);
     }
   }
 
-  setMember = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps): void => {
-    const { members } = this.props;
+  const setMember = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps): void => {
+    const { members } = props;
     const memberOptions = membersToOptions(members);
 
-    this.setState({ queueMember: memberOptions.filter(c => c.value === data.value)[0] });
+    setQueueMember(memberOptions.filter(c => c.value === data.value)[0]);
   }
 
-  render() {
-    const { setStance, setMember } = this;
-    const { members, caucus, caucusFref } = this.props;
-    const { queueMember } = this.state;
+  const memberOptions = membersToOptions(members);
+  const duration = recoverDuration(caucus);
+  const disableButtons = !queueMember || !duration;
 
-    const memberOptions = membersToOptions(members);
-
-    const duration = recoverDuration(caucus);
-    const disableButtons = !queueMember || !duration;
-
-    return (
-      <Segment textAlign="center">
-        <Label attached="top left" size="large">Queue</Label>
-        <Form>
-          <Form.Dropdown
-            icon="search"
-            value={queueMember ? queueMember.value : undefined}
-            search
-            selection
-            loading={!caucus}
-            error={!queueMember}
-            onChange={setMember}
-            options={memberOptions}
+  return (
+    <Segment textAlign="center">
+      <Label attached="top left" size="large">Queue</Label>
+      <Form>
+        <Form.Dropdown
+          icon="search"
+          value={queueMember ? queueMember.value : undefined}
+          search
+          selection
+          loading={!caucus}
+          error={!queueMember}
+          onChange={setMember}
+          options={memberOptions}
+        />
+        <TimerSetter
+          loading={!caucus}
+          unitValue={recoverUnit(caucus)}
+          placeholder="Speaking time"
+          durationValue={duration ? duration.toString() : undefined}
+          onDurationChange={validatedNumberFieldHandler(caucusFref, 'speakerDuration')}
+          onUnitChange={dropdownHandler(caucusFref, 'speakerUnit')}
+        />
+        <Form.Checkbox
+          label="Delegates can queue"
+          indeterminate={!caucus}
+          toggle
+          checked={caucus ? (caucus.queueIsPublic || false) : false} // zoo wee mama
+          onChange={checkboxHandler<CaucusData>(caucusFref, 'queueIsPublic')}
+        />
+        <Button.Group size="large" fluid>
+          <Button
+            content="For"
+            disabled={disableButtons}
+            onClick={setStance(Stance.For)}
           />
-          <TimerSetter
-            loading={!caucus}
-            unitValue={recoverUnit(caucus)}
-            placeholder="Speaking time"
-            durationValue={duration ? duration.toString() : undefined}
-            onDurationChange={validatedNumberFieldHandler(caucusFref, 'speakerDuration')}
-            onUnitChange={dropdownHandler(caucusFref, 'speakerUnit')}
+          <Button.Or />
+          <Button
+            disabled={disableButtons}
+            content="Neutral"
+            onClick={setStance(Stance.Neutral)}
           />
-          <Form.Checkbox
-            label="Delegates can queue"
-            indeterminate={!caucus}
-            toggle
-            checked={caucus ? (caucus.queueIsPublic || false) : false} // zoo wee mama
-            onChange={checkboxHandler<CaucusData>(caucusFref, 'queueIsPublic')}
+          <Button.Or />
+          <Button
+            disabled={disableButtons}
+            content="Against"
+            onClick={setStance(Stance.Against)}
           />
-          <Button.Group size="large" fluid>
-            <Button
-              content="For"
-              disabled={disableButtons}
-              onClick={setStance(Stance.For)}
-            />
-            <Button.Or />
-            <Button
-              disabled={disableButtons}
-              content="Neutral"
-              onClick={setStance(Stance.Neutral)}
-            />
-            <Button.Or />
-            <Button
-              disabled={disableButtons}
-              content="Against"
-              onClick={setStance(Stance.Against)}
-            />
-          </Button.Group>
-        </Form>
-      </Segment>
-    );
-  }
+        </Button.Group>
+      </Form>
+    </Segment>
+  );
 }
