0a526656fab5a9715afb25ae378adb9c6243ae44
Max Bo <max@maxbo.me>
2019-04-16 16:19:34 +1000

    Bump version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ interface State {
   timerId?: NodeJS.Timer;
 }
 
-export const CLIENT_VERSION = 'v2.17.0';
+export const CLIENT_VERSION = 'v2.18.0';
 
 export const CLIENT_VERSION_LINK = (
   <a href="https://github.com/MaxwellBo/Muncoordinated-2/releases">
