b6d678b8817c085aa7289a90191ee5d29d8c4d40
Max Bo <max@maxbo.me>
2019-04-27 13:41:59 +1000

    Display a 404 for invalid strawpolls

--- src/components/Strawpoll.tsx
+++ src/components/Strawpoll.tsx
@@ -6,11 +6,12 @@ import { URLParameters, Dictionary } from '../types';
 import { getStrawpollRef } from '../actions/strawpollActions';
 import { useObject } from 'react-firebase-hooks/database';
 import { Container, Header, Input, Button, List, Icon, Checkbox, Form, CheckboxProps, Progress } from 'semantic-ui-react';
-import { fieldHandler, validatedNumberFieldHandler, clearableZeroableValidatedNumberFieldHandler } from '../actions/handlers';
+import { fieldHandler, clearableZeroableValidatedNumberFieldHandler } from '../actions/handlers';
 import Loading from './Loading';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useLocalStorage, uuidv4 } from '../utils';
 import { StrawpollShareHint } from './ShareHint';
+import { NotFound } from './NotFound';
 
 enum StrawpollStage {
   Preparing = 'preparing',
@@ -84,23 +85,37 @@ function getNumberOfVotes(option: StrawpollOptionData, medium: StrawpollMedium) 
 export default function Strawpoll(props: StrawpollProps) {
     const { committeeID, strawpollID } = props.match.params;
     const strawpollFref = getStrawpollRef(committeeID, strawpollID)
-    const { error, loading, value: strawpoll } = useObject(strawpollFref);
-    const stage: StrawpollStage = strawpoll ? strawpoll.val().stage || StrawpollStage.Results : StrawpollStage.Voting;
-    const medium: StrawpollMedium = strawpoll ? strawpoll.val().medium || StrawpollMedium.Link : StrawpollMedium.Link;
-    const options: Dictionary<StrawpollOptionID, StrawpollOptionData> = 
-      strawpoll ? strawpoll.val().options || {}: {};
+    const { error, loading, value } = useObject(strawpollFref);
     const { initialising, user } = useAuthState(firebase.auth());
+    const [voterID, setVoterID] = useLocalStorage('voterID', undefined);
+
+    if (!voterID) {
+      setVoterID(uuidv4())
+    }
+
+
+    if (loading) {
+      return <Loading />
+    }
+
+    const strawpoll: StrawpollData | undefined = value ? value.val() : undefined;
+
+    if (!strawpoll) {
+      return <Container text>
+        <NotFound id={strawpollID} item="strawpoll" />
+      </Container>;
+    }
+
+    const stage: StrawpollStage = strawpoll ? strawpoll.stage || StrawpollStage.Results : StrawpollStage.Voting;
+    const medium: StrawpollMedium = strawpoll ? strawpoll.medium || StrawpollMedium.Link : StrawpollMedium.Link;
+    const options: Dictionary<StrawpollOptionID, StrawpollOptionData> = 
+      strawpoll ? strawpoll.options || {}: {};
 
     let totalVotes = 0;
 
     Object.keys(options).forEach(oid => {
       totalVotes += getNumberOfVotes(options[oid], medium)
     })
-
-    const [voterID, setVoterID] = useLocalStorage('voterID', undefined);
-    if (!voterID) {
-      setVoterID(uuidv4())
-    }
 
     const addOption = () => {
       strawpollFref.child('options').push(DEFAULT_STRAWPOLL_OPTION);
@@ -277,11 +292,12 @@ export default function Strawpoll(props: StrawpollProps) {
     return renderOption(key, options[key])
   })
 
-  return !loading ? (
+
+  return (
       <Container text style={{ padding: '1em 0em' }}>
         <Header as="h2">
           <Input
-            value={strawpoll ? strawpoll.val().question : ''}
+            value={strawpoll ? strawpoll.question : ''}
             onChange={fieldHandler<StrawpollData>(strawpollFref, 'question')}
             fluid
             placeholder="Type your question here"
@@ -295,5 +311,5 @@ export default function Strawpoll(props: StrawpollProps) {
         </List>
         {buttons}
       </Container>
-    ) : <Loading />
+    );
 }
