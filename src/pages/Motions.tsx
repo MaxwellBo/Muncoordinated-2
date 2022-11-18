2fbd229ce03a73b2dcef6cade8d41e80347107b3
Julian Wandhoven <38176040+JulianWww@users.noreply.github.com>
2022-11-18 10:34:56 +0100

    changed ternary

--- src/pages/Motions.tsx
+++ src/pages/Motions.tsx
@@ -342,7 +342,7 @@ export class MotionsComponent extends React.Component<Props & Hooks, State> {
               </Button>
             }
           />
-          {procedural(motionData.type)? null :
+          {procedural(motionData.type) &&
             <Popup
               content="Abstain"
               trigger={
