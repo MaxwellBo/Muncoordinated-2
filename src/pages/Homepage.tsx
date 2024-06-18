d038d58f0c82db0b82219f161c3980374d90b004
Geordie <65689460+GeordieEK@users.noreply.github.com>
2024-06-18 14:31:51 +1000

    add fresnel back lol

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -135,63 +135,63 @@ interface MobileContainerState {
   sidebarOpened: boolean;
 }
 
-class MobileContainer extends React.Component<MobileContainerProps, MobileContainerState> {
-  constructor(props: MobileContainerProps) {
-    super(props);
+// class MobileContainer extends React.Component<MobileContainerProps, MobileContainerState> {
+//   constructor(props: MobileContainerProps) {
+//     super(props);
 
-    this.state = {
-      sidebarOpened: false
-    };
-  }
+//     this.state = {
+//       sidebarOpened: false
+//     };
+//   }
 
-  handlePusherClick = () => {
-    const { sidebarOpened } = this.state;
+//   handlePusherClick = () => {
+//     const { sidebarOpened } = this.state;
 
-    if (sidebarOpened) {
-      this.setState({ sidebarOpened: false });
-    }
-  }
+//     if (sidebarOpened) {
+//       this.setState({ sidebarOpened: false });
+//     }
+//   }
 
-  handleToggle = () => {
-    this.setState({ sidebarOpened: !this.state.sidebarOpened });
-  }
+//   handleToggle = () => {
+//     this.setState({ sidebarOpened: !this.state.sidebarOpened });
+//   }
 
-  render() {
-    const { children } = this.props;
-    const { sidebarOpened } = this.state;
+//   render() {
+//     const { children } = this.props;
+//     const { sidebarOpened } = this.state;
 
-    return (
-      <>
-        <Sidebar.Pushable>
-          <Sidebar as={Menu} animation="push" inverted vertical visible={sidebarOpened}>
-            <Menu.Item as="a" active>Home</Menu.Item>
-            <Menu.Item as="a" href="/onboard">Log in</Menu.Item>
-            <Menu.Item as="a" href="/onboard">Sign up</Menu.Item>
-          </Sidebar>
+//     return (
+//       <>
+//         <Sidebar.Pushable>
+//           <Sidebar as={Menu} animation="push" inverted vertical visible={sidebarOpened}>
+//             <Menu.Item as="a" active>Home</Menu.Item>
+//             <Menu.Item as="a" href="/onboard">Log in</Menu.Item>
+//             <Menu.Item as="a" href="/onboard">Sign up</Menu.Item>
+//           </Sidebar>
 
-          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
-            <Segment inverted textAlign="center" style={{ minHeight: 350, padding: '1em 0em' }} vertical>
-              <Container>
-                <Menu inverted pointing secondary size="large">
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name="sidebar" />
-                  </Menu.Item>
-                  <Menu.Item position="right">
-                    <Button as="a" inverted href="/onboard" >Log in</Button>
-                    <Button as="a" inverted href="/onboard" style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile={true} />
-            </Segment>
+//           <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
+//             <Segment inverted textAlign="center" style={{ minHeight: 350, padding: '1em 0em' }} vertical>
+//               <Container>
+//                 <Menu inverted pointing secondary size="large">
+//                   <Menu.Item onClick={this.handleToggle}>
+//                     <Icon name="sidebar" />
+//                   </Menu.Item>
+//                   <Menu.Item position="right">
+//                     <Button as="a" inverted href="/onboard" >Log in</Button>
+//                     <Button as="a" inverted href="/onboard" style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+//                   </Menu.Item>
+//                 </Menu>
+//               </Container>
+//               <HomepageHeading mobile={true} />
+//             </Segment>
 
-            {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-        </> 
-    );
-  }
-}
+//             {children}
+//           </Sidebar.Pusher>
+//         </Sidebar.Pushable>
+//         </> 
+//     );
+//   }
+// }
 
 interface ResponsiveContainerProps {
   children?: React.ReactNode;
@@ -200,7 +200,7 @@ interface ResponsiveContainerProps {
 const ResponsiveContainer = ({ children }: ResponsiveContainerProps) => (
   <React.Fragment>
     <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
+    {/* <MobileContainer>{children}</MobileContainer> */}
   </React.Fragment>
 );
 
