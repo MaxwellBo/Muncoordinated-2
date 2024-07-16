66abb74bb8f20f4b34228169fd7d902d4d02d35d
Max Bo <mjbo@canva.com>
2024-07-16 23:18:05 +1000

    Update copyright

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -329,7 +329,7 @@ export default class Homepage extends React.Component<{}, {
                   <p>Made with <span role="img" aria-label="love">💖</span> by <a href="https://github.com/MaxwellBo">Max Bo</a>, 
                   with assistance from the <a href="https://www.facebook.com/UQUNSA/">UQ United Nations Student Association</a>
                   </p>
-                  <p>Copyright © 2022</p>
+                  <p>Copyright © 2024</p>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
