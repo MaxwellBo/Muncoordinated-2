f8ae76c722b996a3c7ed24fb8ba60955efd0dda1
GeordieEK <geordieek@gmail.com>
2024-08-29 21:19:16 +1000

    update cypress to use import syntax and vite localhost default

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'cypress'
+import plugin from './cypress/plugins/index.js'
 
 export default defineConfig({
   blockHosts: 'www.google-analytics.com',
   projectId: 'zxca1q',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return plugin(on, config)
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
