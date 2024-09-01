1dc769cf37db39864b47ad5f3238e4973c82bc13
GeordieEK <geordieek@gmail.com>
2024-09-01 16:09:01 +1000

    outdir Build for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
