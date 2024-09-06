e703bc3424ec61d3d1c3e171154259cefefd3982
Max Bo <mjbo@canva.com>
2024-09-07 08:00:40 +1000

    Revert "Vite Migration (#204)"
    This reverts commit 21c8c9fc34963087c488489f1c8cbc631a20aa0d.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'cypress'
-import plugin from './cypress/plugins/index.js'
 
 export default defineConfig({
   blockHosts: 'www.google-analytics.com',
   projectId: 'zxca1q',
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return plugin(on, config)
+      return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import { it } from 'vitest';
-import { createRoot } from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
 
 it('renders without crashing', () => {
-  const container = document.createElement('div');
-  const root = createRoot(container!);
-  root.unmount();
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
