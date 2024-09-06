5fc5fc4a709a178511627e0f483dde4857b30a8f
Max Bo <mjbo@canva.com>
2024-09-07 08:07:43 +1000

    Reapply "Vite Migration (#204)"
    This reverts commit e703bc3424ec61d3d1c3e171154259cefefd3982.

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [],
+    }
+  },
+})
