5aa0fa5ea92852fdbd58333b113cea9cac69f633
mahangel <angeliquemah@gmail.com>
2025-01-08 10:56:51 -0500

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -12,6 +12,7 @@ import {
   Statistic,
   Sidebar,
   Visibility,
+  ListItem,
 } from 'semantic-ui-react';
 import { logClickCreateACommitteeButton, logClickLogInButton, logClickSignupButton } from '../modules/analytics';
 import Loading from '../components/Loading';
@@ -22,7 +23,6 @@ interface HomepageHeadingProps {
 }
 
 const REPO_LINK = 'https://github.com/MaxwellBo/Muncoordinated-2';
-const logo = '../../logo.png'
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
@@ -32,33 +32,60 @@ const HomepageHeading = ({ mobile }: HomepageHeadingProps) => (
   <Container text>
     <Header
       as="h1"
-      content="Muncoordinated"
+      content="QMUN"
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
+        fontWeight: 'bold',
         marginBottom: 0,
         marginTop: mobile ? '1.5em' : '3em',
       }}
     />
+    <br />
+    <Grid textAlign="center" vertical>
+      <Grid.Column width={5} style={{ marginTop: '-3em' }}>
+        <Image
+          size="massive"
+          rounded
+          src="src/images/logo_unyc.png"
+        />
+      </Grid.Column>
+      <Grid.Column width={3}>
+        <Image
+          size="massive"
+          rounded
+          src="src/images/logo_qmun.png"
+        />
+      </Grid.Column>
+    </Grid>
     <Header
       as="h2"
-      content="The collaborative browser-based Model UN committee management app"
+      content="Quebec Model United Nations Conference 2025"
       inverted
       style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
+        fontSize: mobile ? '0.5em' : '1.7em',
         fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
+        marginTop: mobile ? '0.5em' : '0.5em',
       }}
     />
+
     <br />
-    <Button as="a" primary size="huge" href="/onboard" onClick={logClickCreateACommitteeButton}>
-      Create a committee
-      <Icon name="arrow right" />
+    <Button as="a" primary size="huge" href="/committees/-MEZXMLXacUeaJyXM4zR">
+      Hub Committee A
+    </Button>
+    <Button as="a" primary size="huge" href="/committees/-MT2m3_0wvJdSHeKrk4-">
+      Hub Committee B
     </Button>
     <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
   </Container>
-)
+);
+
 
 interface DesktopContainerProps {
   children?: React.ReactNode;
@@ -108,7 +135,11 @@ class DesktopContainer extends React.Component<DesktopContainerProps, DesktopCon
               size="large"
             >
               <Container>
-                <Menu.Item as="a" active>Home</Menu.Item>
+              <Menu.Item as="a" active>Home</Menu.Item>
+                <Menu.Item as="a" href="/committees/-MEZXMLXacUeaJyXM4zR">QMUN Hub</Menu.Item>
+
+                <Menu.Item as="a" href="/guides">Background Guides</Menu.Item>
+
                 <Menu.Item position="right">
                   <Button as="a" href="/onboard" inverted={!fixed} onClick={logClickLogInButton}>
                     Log in
@@ -136,63 +167,63 @@ interface MobileContainerState {
   sidebarOpened: boolean;
 }
 
-// class MobileContainer extends React.Component<MobileContainerProps, MobileContainerState> {
-//   constructor(props: MobileContainerProps) {
-//     super(props);
+class MobileContainer extends React.Component<MobileContainerProps, MobileContainerState> {
+  constructor(props: MobileContainerProps) {
+    super(props);
 
-//     this.state = {
-//       sidebarOpened: false
-//     };
-//   }
+    this.state = {
+      sidebarOpened: false
+    };
+  }
 
-//   handlePusherClick = () => {
-//     const { sidebarOpened } = this.state;
+  handlePusherClick = () => {
+    const { sidebarOpened } = this.state;
 
-//     if (sidebarOpened) {
-//       this.setState({ sidebarOpened: false });
-//     }
-//   }
+    if (sidebarOpened) {
+      this.setState({ sidebarOpened: false });
+    }
+  }
 
-//   handleToggle = () => {
-//     this.setState({ sidebarOpened: !this.state.sidebarOpened });
-//   }
+  handleToggle = () => {
+    this.setState({ sidebarOpened: !this.state.sidebarOpened });
+  }
 
-//   render() {
-//     const { children } = this.props;
-//     const { sidebarOpened } = this.state;
-
-//     return (
-//       <>
-//         <Sidebar.Pushable>
-//           <Sidebar as={Menu} animation="push" inverted vertical visible={sidebarOpened}>
-//             <Menu.Item as="a" active>Home</Menu.Item>
-//             <Menu.Item as="a" href="/onboard">Log in</Menu.Item>
-//             <Menu.Item as="a" href="/onboard">Sign up</Menu.Item>
-//           </Sidebar>
-
-//           <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
-//             <Segment inverted textAlign="center" style={{ minHeight: 350, padding: '1em 0em' }} vertical>
-//               <Container>
-//                 <Menu inverted pointing secondary size="large">
-//                   <Menu.Item onClick={this.handleToggle}>
-//                     <Icon name="sidebar" />
-//                   </Menu.Item>
-//                   <Menu.Item position="right">
-//                     <Button as="a" inverted href="/onboard" >Log in</Button>
-//                     <Button as="a" inverted href="/onboard" style={{ marginLeft: '0.5em' }}>Sign Up</Button>
-//                   </Menu.Item>
-//                 </Menu>
-//               </Container>
-//               <HomepageHeading mobile={true} />
-//             </Segment>
-
-//             {children}
-//           </Sidebar.Pusher>
-//         </Sidebar.Pushable>
-//         </> 
-//     );
-//   }
-// }
+  render() {
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
+  
+    return (
+      <>
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation="push" inverted vertical visible={sidebarOpened}>
+            <Menu.Item as="a" active>Home</Menu.Item>
+            <Menu.Item as="a" href="/onboard">Log in</Menu.Item>
+            <Menu.Item as="a" href="/onboard">Sign up</Menu.Item>
+          </Sidebar>
+  
+          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
+            <Segment inverted textAlign="center" style={{ minHeight: 350, padding: '1em 0em' }} vertical>
+              <Container>
+                <Menu inverted pointing secondary size="large">
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name="sidebar" />
+                  </Menu.Item>
+                  <Menu.Item position="right">
+                    <Button as="a" inverted href="/onboard">Log in</Button>
+                    <Button as="a" inverted href="/onboard" style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <HomepageHeading mobile={true} />
+            </Segment>
+  
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </>
+    );
+  }
+}
 
 interface ResponsiveContainerProps {
   children?: React.ReactNode;
@@ -201,7 +232,6 @@ interface ResponsiveContainerProps {
 const ResponsiveContainer = ({ children }: ResponsiveContainerProps) => (
   <React.Fragment>
     <DesktopContainer>{children}</DesktopContainer>
-    {/* <MobileContainer>{children}</MobileContainer> */}
   </React.Fragment>
 );
 
@@ -232,105 +262,59 @@ export default class Homepage extends React.Component<{}, {
   render() {
     return (
       <ResponsiveContainer>
-        <Segment style={{ padding: '3em 0em' }} vertical>
-          <Grid container stackable verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header as="h3" style={{ fontSize: '2em' }}>Collaborative</Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  Using a shareable link delegates can: <br />
-                  <ShareCapabilities />
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                  Everyone will see all updates in real-time, without needing to refresh the page. It's like Google Docs, but for MUN.
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                 For virtual MUNs, we recommend pairing Muncoordinated with <a href="https://discord.com/">Discord</a>, which allows you to speak, pass notes, &amp; share files and links.
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                  If you've got a big committee, multiple directors can manage it at the same time, using the same account.
-                </p>
-                <Header as="h3" style={{ fontSize: '2em' }}>Backed up to the cloud</Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  You won't have to worry about data loss ever again. All committee activity is automatically saved to the server, 
-                  so you can start sessions with all data available from the day before.
-                </p>
-              </Grid.Column>
-              <Grid.Column floated="right" width={8}>
-                <Image
-                  bordered
-                  rounded
-                  size="massive"
-                  src="/promo.png"
-                />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header as="h3" style={{ fontSize: '2em' }}>A comprehensive feature set</Header>
-                <div style={{ fontSize: '1.33em' }}>
-                  Muncoordinated supports: <br />
-                  <List as="ul">
-                    <List.Item as="li">Moderated and unmoderated caucuses</List.Item>
-                    <List.Item as="li">Resolutions and amendments</List.Item>
-                    <List.Item as="li">Motions</List.Item>
-                    <List.Item as="li">Roll-call voting</List.Item>
-                    <List.Item as="li">Custom delegations</List.Item>
-                    <List.Item as="li">Strawpolls</List.Item>
-                    <List.Item as="li">File uploads</List.Item>
-                    <List.Item as="li">Delegate performance statistics</List.Item>
-                  </List>
-                </div>
-                <Header as="h3" style={{ fontSize: '2em' }}>Free and open-source</Header>
-                <p style={{ fontSize: '1.33em' }}>
-                  All of Muncoordinated's features are available for free, not locked behind paywalls.
-                </p>
-                <p style={{ fontSize: '1.33em' }}>
-                  It's also <a href="https://github.com/MaxwellBo/Muncoordinated-2">open-source</a>, so you're free to customize it to your needs and liking.
-                </p>
-              </Grid.Column>
-              <Grid.Column floated="right" width={8}>
-                <Image
-                  centered
-                  bordered
-                  rounded
-                  size="medium"
-                  src="/mobile6.png"
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
         <Segment inverted vertical style={{ padding: '5em 0em' }}>
           <Container>
             <Grid divided inverted stackable>
               <Grid.Row>
-                <Grid.Column width={3}>
+                <Grid.Column width={5}>
                   <Header inverted as="h4" content="About" />
                   <List link inverted>
-                    <List.Item as="a" href={REPO_LINK}>Source</List.Item>
+                    <List.Item as="a" href={REPO_LINK}>
+                      Source
+                    </List.Item>
                     <List.Item
                       as="a"
                       href="https://github.com/MaxwellBo/Muncoordinated-2/blob/master/LICENSE"
                     >
                       License
                     </List.Item>
+                    <List.Item as="a" href="mailto:unyc.contact@gmail.com">
+                      unyc.contact@gmail.com
+                    </List.Item>
                   </List>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                  <Header inverted as="h4" content="Services" />
-                  <List link inverted>
-                    <List.Item as="a" href="https://github.com/MaxwellBo/Muncoordinated-2/discussions">Forum</List.Item>
-                    <List.Item as="a" href="https://github.com/MaxwellBo/Muncoordinated-2/issues">Support</List.Item>
-                    <List.Item as="a" href="https://www.helpmymun.com/">MUN Resources</List.Item>
-                  </List>
+                  <div style={{ display: 'flex', gap: '1em', marginTop: '1em' }}>
+                    <a
+                      href="https://www.instagram.com/un.yc/?hl=en"
+                    >
+                      Instagram
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/company/united-nations-youth-of-canada-unyc/posts/?feedView=all"
+                    >
+                      LinkedIn
+                    </a>
+                    <a
+                      href="https://www.facebook.com/unyc.contact"
+                    >
+                      Facebook
+                    </a>
+                  </div>
                 </Grid.Column>
                 <Grid.Column width={7}>
-                  <Header as="h4" inverted>Info</Header>
-                  <p>Made with <span role="img" aria-label="love">💖</span> by <a href="https://github.com/MaxwellBo">Max Bo</a>, 
-                  with assistance from the <a href="https://www.facebook.com/UQUNSA/">UQ United Nations Student Association</a>
+                  <Header as="h4" inverted>
+                    Info
+                  </Header>
+                  <p>
+                    Made by{' '}
+                    <a href="https://github.com/mahangel" >
+                      Angelique Mah
+                    </a>
+                    , adapted from Muncoordinated by{' '}
+                    <a href="https://github.com/MaxwellBo" >
+                      Max Bo
+                    </a>
                   </p>
-                  <p>Copyright © 2024</p>
+                  <p>Copyright © 2025 - Published under the GNU license.</p>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
@@ -339,4 +323,4 @@ export default class Homepage extends React.Component<{}, {
       </ResponsiveContainer>
     );
   }
-}
+}  
